b29b67e848ff4b213f5b73f8e18bf68fd9e18a5f
Daniel Fernandez <danielanthonyharb@gmail.com>
2020-04-21 17:27:52 +1000

    added Readme file with image

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Terminal App in Ruby that replicates an authentic Blackjack experience.
+
+![](C:\Users\danie\projects\Terminal_Project\Images\Flow chart.png)
+
--- Test.rb
+++ Test.rb
@@ -1,6 +1,7 @@
 class Deck
     def initialize
-    @deck = [2,3,4,5,6,7,8,9,10,10,10,10,11,
+    @suit = ['clubs', 'spades', 'hearts', 'diamonds']
+    @deck =[2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
@@ -8,13 +9,16 @@ class Deck
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,]
-            puts "your card is #{get_random_card}"
+            puts "your card is #{deal}"
+            
+    
             
     end
 end
 
-def get_random_card
-    @deck[rand(@deck.length)]
+def deal
+    @deck[rand(@deck.length)] 
+    # @suit[rand(@suit.length)]
 end 
     
 Deck.new
