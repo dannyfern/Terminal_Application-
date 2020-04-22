7e1c9ec5e6c11b3a9df2679c6ba4f4590e43a51b
Daniel Fernandez <danielanthonyharb@gmail.com>
2020-04-22 17:02:48 +1000

    Revised code, two steps backwards one step forward so i can regain functionality of the value method

--- Test.rb
+++ Test.rb
@@ -12,34 +12,58 @@ class Deck
             2,3,4,5,6,7,8,9,10,10,10,10,11,]
     @player_hand = starting_hand
     @dealer_hand = starting_hand
-    puts "Players hand is #{@player_hand}"
-    puts "Dealer hand is #{@dealer_hand}"
+     puts "Players hand is #{@player_hand} Value: #{get_hand_value(@player_hand)}"
+     puts "Dealer hand is #{@dealer_hand} Value:  #{get_hand_value(@dealer_hand)}"
     end
 end
 
-#Return the intitial hand for later use on hit function
 def starting_hand
-    [get_hand, get_hand]
+    [get_random_card, get_random_card]
 end 
 
-#Initialize output of deck randomly and supply it with parameters for the value of card chosen.
-def get_hand
-    deck_number = @deck[rand(@deck.length)] 
-    suit_number = @suit[rand(@suit.length)]
-    picture_number = @picture[rand(@picture.length)]
-    if deck_number == 10
-        return deck_number, suit_number, picture_number
-    elsif deck_number == 11
-        return deck_number, suit_number, 'Ace'
-    else
-        return deck_number, suit_number
-    end
+def get_hand_value(hand)
+    sum = 0
+    hand.each do |card|
+        sum = sum + card
+    end 
+    sum
 end 
 
-#Return the sum of the hand to a value
+def get_random_card
+    deck_number = @deck.sample
+end
+
+
+ 
 
 
 Deck.new
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def get_hand
+#     deck_number = @deck[rand(@deck.length)] 
+#     suit_number = @suit[rand(@suit.length)]
+#     picture_number = @picture[rand(@picture.length)]
+#     if deck_number == 10
+#         return deck_number, suit_number, picture_number
+#     elsif deck_number == 11
+#         return deck_number, suit_number, 'Ace'
+#     else
+#         return deck_number, suit_number
+#     end
+# end 
 
 
 
--- cards.rb
+++ cards.rb
@@ -1,6 +1,6 @@
 class Deck
     def initialize
-    @suit = ['clubs', 'spades', 'hearts', 'diamonds']
+    @suit = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
     @picture = ['Jack', 'Queen', 'King']
     @deck =[2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
@@ -10,23 +10,59 @@ class Deck
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,]
-            puts "your card is #{deal}" 
+    @player_hand = starting_hand
+    @dealer_hand = starting_hand
+     puts "Players hand is #{@player_hand} Value: #{get_hand_value(@player_hand)}"
+     puts "Dealer hand is #{@dealer_hand} Value:  #{get_hand_value(@dealer_hand)}"
     end
 end
-#Initialize output of deck randomly and supply it with parameters for the value of card chosen.
-def deal
-    deck_number = @deck[rand(@deck.length)] 
-    suit_number = @suit[rand(@suit.length)]
-    picture_number = @picture[rand(@picture.length)]
-    if deck_number == 10
-        return deck_number, suit_number, picture_number
-    elsif deck_number == 11
-        return deck_number, suit_number, 'Ace'
-    else
-        return deck_number, suit_number
-    end
-    
+
+def starting_hand
+    [get_random_card, get_random_card]
 end 
-    
-deck = Deck.new
-deck1 = Deck.new
+
+def get_hand_value(hand)
+    sum = 0
+    hand.each do |card|
+        sum = sum + card
+    end 
+    sum
+end 
+
+def get_random_card
+    deck_number = @deck.sample
+end
+
+
+ 
+
+
+Deck.new
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def get_hand
+#     deck_number = @deck[rand(@deck.length)] 
+#     suit_number = @suit[rand(@suit.length)]
+#     picture_number = @picture[rand(@picture.length)]
+#     if deck_number == 10
+#         return deck_number, suit_number, picture_number
+#     elsif deck_number == 11
+#         return deck_number, suit_number, 'Ace'
+#     else
+#         return deck_number, suit_number
+#     end
+# end 
+
+
