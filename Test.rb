6b0d8e716b19ad008610babf73e5102f80d7c323
Daniel Fernandez <danielanthonyharb@gmail.com>
2020-04-22 10:38:26 +1000

    Updated Deck with deal, allowed it to recognise picture cards when value is equal to the if else statement

--- Test.rb
+++ Test.rb
@@ -1,6 +1,7 @@
 class Deck
     def initialize
     @suit = ['clubs', 'spades', 'hearts', 'diamonds']
+    @picture = ['Jack', 'Queen', 'King']
     @deck =[2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
@@ -9,17 +10,26 @@ class Deck
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,]
-            puts "your card is #{deal}"
-            
-    
-            
+            puts "your card is #{deal}" 
     end
 end
-
+#Initialize output of deck randomly and supply it with parameters for the value of card chosen.
 def deal
-    @deck[rand(@deck.length)] 
-    # @suit[rand(@suit.length)]
+    deck_number = @deck[rand(@deck.length)] 
+    suit_number = @suit[rand(@suit.length)]
+    picture_number = @picture[rand(@picture.length)]
+    if deck_number == 10
+        return deck_number, suit_number, picture_number
+    elsif deck_number == 11
+        return deck_number, suit_number, 'Ace'
+    else
+        return deck_number, suit_number
+    end
+    
 end 
     
-Deck.new
+deck = Deck.new
+deck1 = Deck.new
+
+
 
--- cards.rb
+++ cards.rb
@@ -1,6 +1,8 @@
 class Deck
     def initialize
-    @deck = [2,3,4,5,6,7,8,9,10,10,10,10,11,
+    @suit = ['clubs', 'spades', 'hearts', 'diamonds']
+    @picture = ['Jack', 'Queen', 'King']
+    @deck =[2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
@@ -8,14 +10,23 @@ class Deck
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,]
-            puts "your card is #{get_random_card}"
-            
+            puts "your card is #{deal}" 
     end
 end
-
-def get_random_card
-    @deck[rand(@deck.length)]
+#Initialize output of deck randomly and supply it with parameters for the value of card chosen.
+def deal
+    deck_number = @deck[rand(@deck.length)] 
+    suit_number = @suit[rand(@suit.length)]
+    picture_number = @picture[rand(@picture.length)]
+    if deck_number == 10
+        return deck_number, suit_number, picture_number
+    elsif deck_number == 11
+        return deck_number, suit_number, 'Ace'
+    else
+        return deck_number, suit_number
+    end
+    
 end 
     
-Deck.new
-
+deck = Deck.new
+deck1 = Deck.new
