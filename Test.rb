9f15e6ee7656b41b1fd6ed17561e92657c215687
Daniel Fernandez <danielanthonyharb@gmail.com>
2020-04-22 12:53:24 +1000

    Added variable for hand

--- Test.rb
+++ Test.rb
@@ -1,6 +1,6 @@
 class Deck
     def initialize
-    @suit = ['clubs', 'spades', 'hearts', 'diamonds']
+    @suit = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
     @picture = ['Jack', 'Queen', 'King']
     @deck =[2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
@@ -10,11 +10,20 @@ class Deck
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,]
-            puts "your card is #{deal}" 
+    @player_hand = starting_hand
+    @dealer_hand = starting_hand
+    puts "Players hand is #{@player_hand}"
+    puts "Dealer hand is #{@dealer_hand}"
     end
 end
+
+#Return the intitial hand for later use on hit function
+def starting_hand
+    [get_hand, get_hand]
+end 
+
 #Initialize output of deck randomly and supply it with parameters for the value of card chosen.
-def deal
+def get_hand
     deck_number = @deck[rand(@deck.length)] 
     suit_number = @suit[rand(@suit.length)]
     picture_number = @picture[rand(@picture.length)]
@@ -25,11 +34,14 @@ def deal
     else
         return deck_number, suit_number
     end
-    
 end 
-    
-deck = Deck.new
-deck1 = Deck.new
+
+#Return the sum of the hand to a value
+
+
+Deck.new
+
+
 
 
 
